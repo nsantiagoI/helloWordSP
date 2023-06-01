18339394f1e085d09939f45f78b44a16648e1ca0
nsantiago <nsantiago@NSANTIA-BYR1RQ3>
2023-06-01 14:54:20 -0300

    Add project files.

---
+++ helloWorldSP_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("helloWorldSP_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("helloWorldSP_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("037E1AE9-DCBF-4E56-9F25-66F449DBD43C")]
+[assembly: AssemblyVersion("1.0.0.0")]
